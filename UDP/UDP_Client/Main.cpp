68de20884610b27be1222f81faaeac4629967081
lsj-mnp <53816963+lsj-mnp@users.noreply.github.com>
2020-04-26 17:02:24 +0900

    [이승중] Client 만듬
    이미 만들었던 프로젝트에서 복사해서 가져옴.

---
+++ UDP/UDP_Client/Main.cpp
@@ -0,0 +1,35 @@
+﻿#include "CClient.h"
+
+int main()
+{
+	CClient client{};
+
+	client.SetServerAddr("192.168.35.60", 15000);
+
+	std::thread thrReceive
+	{
+		[&]
+		{
+			while (true)
+			{
+				if (client.Receive())
+				{
+					printf("%s\n", client.GetBuff());
+				}
+			}
+		}
+	};
+
+	char sendMessage[5000]{};
+
+	while (true)
+	{
+		fgets(sendMessage, 5000, stdin);
+
+		client.Send(sendMessage);
+	}
+
+	thrReceive.join();
+
+	return 0;
+}
